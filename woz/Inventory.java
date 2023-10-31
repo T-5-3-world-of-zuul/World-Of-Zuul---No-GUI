0d270f8937d948ab8cb57f64387f8bb7da5ea820
Ryvy <44634266+Ryvy@users.noreply.github.com>
2023-10-31 14:13:18 +0100

    commit inventory
    Not tested

---
+++ woz/Inventory.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+public class Inventory {
+    public ArrayList<Item> inventory = new ArrayList<>();
+
+    public void AddItemToInventory(Item item){
+        int inventorySlots = 8;
+        if(inventory.size() >= inventorySlots) {
+            return;
+        }
+        inventory.add(item);
+    }
+
+    public void RemoveItemFromInventory(int itemIndex){
+        for (Item item:
+                inventory) {
+            if(item.index == itemIndex)
+                inventory.remove(itemIndex);
+        }
+    }
+}
